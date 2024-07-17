3aac8b7038e46923371f6066a280a3aa28f19042
Wjyeop <wnsduq2178@gmail.com>
2024-07-18 04:48:21 +0900

    feat. NewsLetter 섹션

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,4 +1,5 @@
 import MainSection from "./MainSection";
+import NewsLetterSection from "./NewLetterSection";
 import NewsSection from "./NewSection";
 
 const LandingPage = () => {
@@ -6,6 +7,7 @@ const LandingPage = () => {
     <div>
       <MainSection />
       <NewsSection />
+      <NewsLetterSection />
     </div>
   );
 };
---
+++ src/pages/LandingPage/NewLetterSection.tsx
@@ -0,0 +1,120 @@
+import more from "../../assets/images/more.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+
+const NewsLetterSection = () => {
+  return (
+    <section className="newsletter-section">
+      <div className="title">
+        <div>
+          <p style={{ fontSize: "2.5em" }}>
+            <span className="blue" style={{ fontSize: "2em" }}>
+              N
+            </span>
+            EWS <br />
+            LETTER
+            <button className="more">
+              <img src={more} alt="" />
+            </button>
+          </p>
+        </div>
+      </div>
+      <div className="swiper-container">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          navigation
+          spaceBetween={10}
+          slidesPerView={4}
+          scrollbar={{ draggable: true }}
+          slidesOffsetAfter={200}
+        >
+          <SwiperSlide>
+            <div className="newsletter-content">
+              <div className="mark">
+                <span>회계 법률</span>
+              </div>
+              <div className="title">
+                <span>
+                  '법률AI'거스를 수 없는 대세...변호사 대체 아닌 '협업'으로
+                </span>
+              </div>
+              <div className="bottom">
+                <span className="date">2024.07.01</span>
+                <span className="more">자세히 보기</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="newsletter-content">
+              <div className="mark">
+                <span>회계 법률</span>
+              </div>
+              <div className="title">
+                <span>
+                  '법률AI'거스를 수 없는 대세...변호사 대체 아닌 '협업'으로
+                </span>
+              </div>
+              <div className="bottom">
+                <span className="date">2024.07.01</span>
+                <span className="more">자세히 보기</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="newsletter-content">
+              <div className="mark">
+                <span>회계 법률</span>
+              </div>
+              <div className="title">
+                <span>
+                  '법률AI'거스를 수 없는 대세...변호사 대체 아닌 '협업'으로
+                </span>
+              </div>
+              <div className="bottom">
+                <span className="date">2024.07.01</span>
+                <span className="more">자세히 보기</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="newsletter-content">
+              <div className="mark">
+                <span>회계 법률</span>
+              </div>
+              <div className="title">
+                <span>
+                  '법률AI'거스를 수 없는 대세...변호사 대체 아닌 '협업'으로
+                </span>
+              </div>
+              <div className="bottom">
+                <span className="date">2024.07.01</span>
+                <span className="more">자세히 보기</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="newsletter-content">
+              <div className="mark">
+                <span>회계 법률</span>
+              </div>
+              <div className="title">
+                <span>
+                  '법률AI'거스를 수 없는 대세...변호사 대체 아닌 '협업'으로
+                </span>
+              </div>
+              <div className="bottom">
+                <span className="date">2024.07.01</span>
+                <span className="more">자세히 보기</span>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default NewsLetterSection;
