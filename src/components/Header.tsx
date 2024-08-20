802d26bcfae6681d046f68052b31d782a9d0eeed
Wjyeop <wnsduq2178@gmail.com>
2024-08-20 12:22:31 +0900

    fix. header 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,24 @@
-import React from "react";
 import "../App.css";
 import logo from "../assets/images/HeaderLogo.png";
-import globe from "../assets/images/HeaderGlobe.png";
+import marker from "../assets/images/icons/marker.png";
 import phone from "../assets/images/HeaderPhoneCall.png";
-import map from "../assets/images/HeaderMap.png";
 import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-  const location = useLocation();
-  const isCenterPage =
-    location.pathname === "/support-center" ||
-    location.pathname === "/tech-center";
+  // const location = useLocation();
+  // const isCenterPage =
+  //   location.pathname === "/support-center" ||
+  //   location.pathname === "/tech-center";
 
-  if (isCenterPage) {
-    return (
-      <header className="center-header">
-        <Link to="/">
-          <img src={logo} alt="Logo" className="logo-image" />
-        </Link>
-      </header>
-    );
-  }
+  // if (isCenterPage) {
+  //   return (
+  //     <header className="center-header">
+  //       <Link to="/">
+  //         <img src={logo} alt="Logo" className="logo-image" />
+  //       </Link>
+  //     </header>
+  //   );
+  // }
 
   return (
     <header className="header">
@@ -51,9 +49,9 @@ const Header = () => {
         </nav>
       </div>
       <div className="header-section icons">
-        <img src={globe} alt="globe" className="icon" />
+        <img src={marker} alt="marker" className="icon" />
         <img src={phone} alt="phone" className="icon" />
-        <img src={map} alt="map" className="icon" />
+        {/* <img src={map} alt="map" className="icon" /> */}
       </div>
     </header>
   );
